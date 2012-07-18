919e11285b3d4409acf71ad4b5aa78f345ccdcd5
Andrew Pinski <apinski@cavium.com>
2012-07-18 16:15:50 -0700

    2012-07-18  Andrew Pinski  <apinski@cavium.com>
    	* testsuite/gas/mips/align2-el.d: Allow for 64bit addresses.
    	* testsuite/gas/mips/align2.d: Likewise.
    	* testsuite/gas/mips/loongson-2f-2.d: Likewise.
    	* testsuite/gas/mips/loongson-2f-3.d: Likewise.
    	* testsuite/gas/mips/mips-jalx.d: Likewise.
    	* testsuite/gas/mips/mips16e-64.d: Likewise.
    	* testsuite/gas/mips/mips64-dsp.d: Likewise.
    	* testsuite/gas/mips/set-arch.d: Likewise.
    	* testsuite/gas/mips/octeon-saa-saad.d: Run as 32bit only.

--- gas/testsuite/gas/mips/mips16e-64.d
+++ gas/testsuite/gas/mips/mips16e-64.d
@@ -9,11 +9,11 @@
 
 Disassembly of section .text:
 
-0x00000000 ecd1      	sew	\$4
-0x00000002 ec51      	zew	\$4
-0x00000004 6500      	nop
-0x00000006 6500      	nop
-0x00000008 6500      	nop
-0x0000000a 6500      	nop
-0x0000000c 6500      	nop
-0x0000000e 6500      	nop
+0x0+000000 ecd1      	sew	\$4
+0x0+000002 ec51      	zew	\$4
+0x0+000004 6500      	nop
+0x0+000006 6500      	nop
+0x0+000008 6500      	nop
+0x0+00000a 6500      	nop
+0x0+00000c 6500      	nop
+0x0+00000e 6500      	nop
