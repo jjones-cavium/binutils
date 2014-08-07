8db46cfe1cdff78bb8ef9e729d343c8796511fc3
Andrew Pinski <apinski@cavium.com>
2014-08-07 11:40:36 -0700

    2014-08-07  Andrew Pinski  <apinski@cavium.com>
    	* config/tc-aarch64.c (aarch64_cpus): Rename thunder to thunderx.
    	* testsuite/gas/aarch64/cvmcache.d: Update for the cpu name change of
    	thunder to thunderx.

--- gas/config/tc-aarch64.c
+++ gas/config/tc-aarch64.c
@@ -7316,9 +7316,9 @@ static const struct aarch64_cpu_option_table aarch64_cpus[] = {
   {"all", AARCH64_ANY, NULL},
   {"cortex-a53",	AARCH64_ARCH_V8, "Cortex-A53"},
   {"cortex-a57",	AARCH64_ARCH_V8, "Cortex-A57"},
-  {"thunder",		AARCH64_ARCH_V8 | AARCH64_FEATURE_CRYPTO \
+  {"thunderx",		AARCH64_ARCH_V8 | AARCH64_FEATURE_CRYPTO \
 			| AARCH64_FEATURE_CRC | AARCH64_FEATURE_ATOMIC \
-			| AARCH64_FEATURE_CACHE, "Cavium Thunder"},
+			| AARCH64_FEATURE_CACHE, "Cavium ThunderX"},
   {"xgene-1",		AARCH64_ARCH_V8, "APM X-Gene 1"},
   {"generic", AARCH64_ARCH_V8, NULL},
 
